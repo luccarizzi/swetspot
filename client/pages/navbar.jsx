f3c79c40fb774ea4608575bfe14e8aa391d3307e
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-08 23:15:09 +0000

    WIP. Add a burger icon to the left side of the page (on phone and tablet). #1

--- client/pages/navbar.jsx
+++ client/pages/navbar.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import logo from '../../server/public/src/logo.png';
+import MediaQuery from 'react-responsive';
 
-function Navbar() {
-  return (
-    <div className='container-fluid'>
-      <div className='row'>
-        <div className='col-3 col-md-2 col-lg-1'>
-          <a href='#'>
-            <img
-              className='img-fluid'
-              src={logo}
-              alt='swētSPOT logo.'></img>
-          </a>
+export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-3 col-md-2 col-lg-1'>
+            <a href='#'>
+              <img
+                className='img-fluid'
+                src={logo}
+                alt='swētSPOT logo.'></img>
+            </a>
+          </div>
+          <div>
+            <div>
+              <MediaQuery minDeviceWidth={500}>
+                {(matches) =>
+                  matches
+                    ? <p>You are retina</p>
+                    : <p>You are not retina</p>
+                }
+              </MediaQuery>
+            </div>
+          </div>
         </div>
-        {/* <div className='col-6'>
-          <ul>
-            <li>Services</li>
-            <li></li>
-          </ul>
-        </div> */}
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Navbar;
