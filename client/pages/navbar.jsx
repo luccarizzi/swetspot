fe8e7c9231284abefb6ec7b724e872c64af4576e
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-08 21:26:41 +0000

    Add the logo to the right side of the page. #1

--- client/pages/navbar.jsx
+++ client/pages/navbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import logo from '../../server/public/src/logo.png';
 
 function Navbar() {
   return (
-    <h1>Navbar</h1>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-3 col-md-2 col-lg-1'>
+          <a href='#'>
+            <img
+              className='img-fluid'
+              src={logo}
+              alt='swētSPOT logo.'></img>
+          </a>
+        </div>
+        {/* <div className='col-6'>
+          <ul>
+            <li>Services</li>
+            <li></li>
+          </ul>
+        </div> */}
+      </div>
+    </div>
   );
 }
 
