95a61a25444ee2fc511ae40f2cac24bdd2ce6285
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-11 22:35:22 +0000

    Create a Service title. #1

--- client/pages/services.jsx
+++ client/pages/services.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function Services() {
   return (
-    <h1>Services</h1>
+    <div className='container-fluid py-4 my-1'>
+      <div className='row'>
+        <div className='col text-center'>
+          <h1 className='f-style-kanit display-1 my-4 red-2'>Services</h1>
+        </div>
+      </div>
+    </div>
   );
 }
 
