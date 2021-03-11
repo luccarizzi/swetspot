8f9a2cdae8304413b436528ef0b9aceba239fb9c
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-11 22:49:51 +0000

    WIP Create a card for each service. #1

--- client/pages/banner.jsx
+++ client/pages/banner.jsx
@@ -5,7 +5,7 @@ function Banner() {
     <div className='container-fluid banner text-white py-4 my-1'>
       <div className='row'>
         <div className='col text-center'>
-          <h1 className='f-style-kanit display-1 my-4'>Professional Services</h1>
+          <h1 className='f-style-kanit display-1 my-4 text-shadow'>Professional Services</h1>
           <p className='h3 f-style-confortaa fw-lighter my-4'>
             We put the <span className='f-style-lobster f-letter-spacing-1 green-1'>Cherry</span> on top of tech!
           </p>
--- client/pages/services.jsx
+++ client/pages/services.jsx
@@ -5,7 +5,18 @@ function Services() {
     <div className='container-fluid py-4 my-1'>
       <div className='row'>
         <div className='col text-center'>
-          <h1 className='f-style-kanit display-1 my-4 red-2'>Services</h1>
+          <h1 className='f-style-kanit display-1 my-1 red-2 text-shadow'>Services</h1>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-12 col-sm-6 col-md-4'>
+          test
+        </div>
+        <div className='col-12 col-sm-6 col-md-4'>
+          test
+        </div>
+        <div className='col-12 col-md-4'>
+          test
         </div>
       </div>
     </div>
