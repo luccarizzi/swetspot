31f6ba5a5ce105e136f224bfd1b9f4053226413f
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-11 21:48:40 +0000

    Add a background to the Banner component and make it responsive stretching from each side of the page. #1

--- client/pages/banner.jsx
+++ client/pages/banner.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 function Banner() {
   return (
-    <h1>Banner</h1>
+    <div className='container-fluid banner text-white py-4 my-1'>
+      <div className='row'>
+        <div className='col text-center'>
+          <h1 className='f-style-kanit display-1 p-4'>Professional Services</h1>
+          <p className='h3 f-style-confortaa fw-lighter p-2'>
+            We put the <span className='f-style-lobster f-letter-spacing-1 green-1'>Cherry</span> on top of tech!
+          </p>
+          <div className='p-2'>
+            <a href='#' className=''>
+              Quote
+          </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
