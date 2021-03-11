10d46ca2ca9afb075f37059f98200bd802c3a40e
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-11 22:08:36 +0000

    Add a Get Quote button. #1

--- client/pages/banner.jsx
+++ client/pages/banner.jsx
@@ -5,14 +5,16 @@ function Banner() {
     <div className='container-fluid banner text-white py-4 my-1'>
       <div className='row'>
         <div className='col text-center'>
-          <h1 className='f-style-kanit display-1 p-4'>Professional Services</h1>
-          <p className='h3 f-style-confortaa fw-lighter p-2'>
+          <h1 className='f-style-kanit display-1 my-4'>Professional Services</h1>
+          <p className='h3 f-style-confortaa fw-lighter my-4'>
             We put the <span className='f-style-lobster f-letter-spacing-1 green-1'>Cherry</span> on top of tech!
           </p>
-          <div className='p-2'>
-            <a href='#' className=''>
-              Quote
-          </a>
+          <div className='my-4'>
+            <a href='#' className='f-style-confortaa text-white text-decoration-none h3'>
+              <div className='button'>
+                Get Quote
+              </div>
+            </a>
           </div>
         </div>
       </div>
