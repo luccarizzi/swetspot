9a941592d3a554cb13cff49ed1b0d0cc30e1935f
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-15 22:04:20 +0000

    Create a class within Dropdown with constructor method and a state with dropdown as its only property. The state dropdown property should be set to false. #2

--- client/components/dropdown.jsx
+++ client/components/dropdown.jsx
@@ -1,1 +1,14 @@
 import React from 'react';
+
+export default class Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: false
+    }
+  }
+
+  render() {
+    return ()
+  }
+}
