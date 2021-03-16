47ca1a6740c77f0e3a8d94b086b5a7f6765e9f45
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-16 12:33:59 +0000

    Within BlogPage create a class and export it. #3

--- client/pages/blog-page.jsx
+++ client/pages/blog-page.jsx
@@ -1,3 +1,16 @@
 import React from 'react';
 import Navbar from './navbar';
 import Footer from './footer';
+
+export default class BlogPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
