0e16aa6027e5b978ada02fedfd845c5e07d88d08
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-16 14:49:37 +0000

    Add the tables to schema.sql. #3

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,15 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "posts" (
+	"postId" serial NOT NULL,
+	"title" TEXT NOT NULL,
+	"date" TEXT NOT NULL,
+	"image" TEXT NOT NULL,
+	"text" TEXT NOT NULL,
+	"preview" TEXT NOT NULL,
+	CONSTRAINT "posts_pk" PRIMARY KEY ("postId")
+) WITH (
+  OIDS=FALSE
+);
