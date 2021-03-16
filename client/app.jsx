654abf46d82d4199dfeb1a56f17d6fd08bca9c0e
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-16 13:17:23 +0000

    Add the componentDidMount method and listen for the hashchange event. #3

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import Home from './pages/home';
 import BlogPage from './pages/blog-page';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidCatch() {
+    window.addEventListener('hashchange', event => {
+      const newRoute = parseRoute(window.location.hash);
+      this.setState({
+        route: newRoute
+      });
+    });
+  }
+
   render() {
     return (
       <Home />
