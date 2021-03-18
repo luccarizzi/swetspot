becb239ff426310813b83e7878e5c689dc7e09dd
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-18 12:32:34 +0000

    Create the endpoint to request the post data. #4

--- server/index.js
+++ server/index.js
@@ -21,15 +21,47 @@ app.get('/api/blog-page', (req, res, next) => {
   const sql = `
     select "postId", "title", "date", "image", "preview"
     from "posts"
-  `
+  `;
   db.query(sql)
     .then(result => res.json(result.rows))
     .catch(err => next(err));
-})
+});
 
-app.get('/api/blog-page/post/:postId', (req, res, next) => {
-  res.send(req.params.postId)
-})
+app.get('/api/blog-page/:postId', (req, res, next) => {
+  const postId = parseInt(req.params.postId, 10);
+  if (postId < 1 || !postId) {
+    throw new ClientError(400, 'postId must be a positive integer');
+  }
+  const data = {};
+  const params = [postId];
+  const sqlPost = `
+    select "postId", "title", "date", "image", "author"
+    from "posts"
+    where "postId" = $1
+  `;
+  const sqlParagraph = `
+    select "paragraphId", "text"
+    from "postParagraph"
+    join "paragraphs" using ("paragraphId")
+    where "postId" = $1
+  `;
+  db
+    .query(sqlPost, params)
+    .then(result => {
+      if (!result.rows[0]) {
+        throw new ClientError(404, `cannot find post with postId ${postId}`);
+      }
+      data.post = result.rows;
+      return db.query(sqlParagraph, params);
+    })
+    .then(result => {
+      data.paragraph = result.rows;
+      res.json(data);
+    })
+    .catch(err => next(err));
+});
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
