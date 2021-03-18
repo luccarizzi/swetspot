30d8ccbfb2b3376e8080e7d999d12763137cf810
Lucca Rizzi <luccarizzi29@gmail.com>
2021-03-18 17:10:33 +0000

    Create a fetch requests within Blog component. #5

--- client/pages/blog.jsx
+++ client/pages/blog.jsx
@@ -1,24 +1,57 @@
 import React from 'react';
 
-function Blog() {
-  return (
-    <div id='blog' className='container-fluid bg-red text-white py-4'>
-      <div className='row'>
-        <div className='col text-center'>
-          <h1 className='f-style-kanit display-1 my-4 text-shadow'>Tech Cherry</h1>
-        </div>
-      </div>
-      <div className='row text-center'>
-        <div className='my-lg-4'>
-          <a href='#blog-page' className='f-style-confortaa text-white text-decoration-none h3'>
-            <div className='button b-white'>
-              See All
-            </div>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-}
+export default class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ''
+    };
+  }
 
-export default Blog;
+  componentDidMount() {
+    fetch('/api/blog')
+      .then(res => res.json())
+      .then(data => this.setState({
+        data
+      }));
+  }
+
+  renderBlogPostPreview() {
+    const { data } = this.state;
+    if (data) {
+      return (
+        <>
+
+        </>
+      );
+    } else {
+      return (
+        <div className="loader">Loading...</div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div id='blog' className='container-fluid bg-red text-white py-4'>
+        <div className='row'>
+          <div className='col-12 text-center'>
+            <h1 className='f-style-kanit display-1 my-4 text-shadow'>Tech Cherry</h1>
+          </div>
+        </div>
+        <div>
+
+        </div>
+        <div className='row text-center'>
+          <div className='col-12 my-lg-4'>
+            <a href='#blog-page' className='f-style-confortaa text-white text-decoration-none h3'>
+              <div className='button b-white'>
+                See All
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
